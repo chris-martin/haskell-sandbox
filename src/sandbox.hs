d1400354eacd7f77de44c6ee21e9f85d67625a41
Chris Martin <ch.martin@gmail.com>
2016-04-03 19:58:26 -0700

    Identity Monad

--- src/sandbox.hs
+++ src/sandbox.hs
@@ -1,33 +1,32 @@
 module Main (main) where
 
-import           Prelude                  hiding (Left, Right)
+import           Prelude
 
 import           Test.QuickCheck
 import           Test.QuickCheck.Checkers
 import           Test.QuickCheck.Classes
 
+type X = Int
+
 main :: IO ()
 main = do
-  let t = undefined :: PhhhbbtttEither String (Int, String, Char)
+  let t = undefined :: Identity (X, X, X)
   mapM_ quickBatch [functor t, applicative t, monad t]
 
-data PhhhbbtttEither b a = Left a | Right b deriving (Eq, Show)
+newtype Identity a = Identity a deriving (Eq, Ord, Show)
 
-instance (Eq a, Show a, Eq b, Show b) => EqProp (PhhhbbtttEither b a) where
+instance (Eq a, Show a) => EqProp (Identity a) where
   (=-=) = eq
 
-instance (Arbitrary a, Arbitrary b) => Arbitrary (PhhhbbtttEither b a) where
-  arbitrary = oneof [Left <$> arbitrary, Right <$> arbitrary]
+instance Arbitrary a => Arbitrary (Identity a) where
+  arbitrary = Identity <$> arbitrary
 
-instance Functor (PhhhbbtttEither b) where
-  fmap f (Left b) = Left (f b)
-  fmap _ (Right a) = Right a
+instance Functor Identity where
+  fmap f (Identity x) = Identity (f x)
 
-instance Applicative (PhhhbbtttEither a) where
-  pure = Left
-  Right x <*> _ = Right x
-  Left  f <*> x = fmap f x
+instance Applicative Identity where
+  pure = Identity
+  Identity f <*> Identity x = Identity (f x)
 
-instance Monad (PhhhbbtttEither a) where
-  (Left x)  >>= f = f x
-  (Right x) >>= _ = Right x
+instance Monad Identity where
+  Identity x >>= f = f x
