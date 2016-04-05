b7b4e3d23ae4997e7a246ae9949594a2e9c08f0f
Chris Martin <ch.martin@gmail.com>
2016-04-04 20:39:23 -0700

    expressions
    https://stackoverflow.com/questions/36414373/

--- src/sandbox.hs
+++ src/sandbox.hs
@@ -1,23 +1,45 @@
-module Main (main, l1, l2, a, meh, flipType) where
+module Main (main, Expr(..), beta) where
 
 import           Test.QuickCheck
 import           Test.QuickCheck.Checkers
 import           Test.QuickCheck.Classes
 
+import qualified Data.List
+import qualified Data.Maybe
+
 main :: IO ()
 main = return ()
 
-l1 :: Monad m => (a -> b) -> m a -> m b
-l1 = (<$>)
+data Expr = C Float | Expr :+ Expr | Expr :- Expr | Expr :* Expr | Expr :/ Expr
+          | Var String | Let [(String, Expr)] Expr
 
-l2 :: Monad m => (a -> b -> c) -> m a -> m b -> m c
-l2 f x y = f <$> x <*> y
+-- | @beta var value e@ replaces variables named @var@ with the value @value@
+-- wherever anywhere that variable occurs in expression @e@.
+beta :: String -> Expr -> Expr -> Expr
 
-a :: Monad m => m a -> m (a -> b) -> m b
-a = flip (<*>)
+-- "let x = y in x" = y
+beta v1 value (Var v2) | v1 == v2 = value
 
-meh :: Monad m => [a] -> (a -> m b) -> m [b]
-meh xs f = flipType (fmap f xs)
+-- "let x = y in z" = z
+beta _ _ e@(Var _) = e
 
-flipType :: (Monad m) => [m a] -> m [a]
-flipType = foldr (\x acc -> (:) <$> x <*> acc) (pure [])
+-- Constants are unaffected
+beta _ _ c@(C _) = c
+
+-- For operators, apply @beta a b@ recursively to the operands.
+beta a b (e1 :+ e2) = (beta a b e1) :+ (beta a b e2)
+beta a b (e1 :- e2) = (beta a b e1) :- (beta a b e2)
+beta a b (e1 :* e2) = (beta a b e1) :* (beta a b e2)
+beta a b (e1 :/ e2) = (beta a b e1) :/ (beta a b e2)
+
+beta a b e@(Let bindings body) =
+
+    -- The variable is shadowed by a let binding, so return
+    -- the original expression unmodified.
+    if bindingsContains a bindings then e
+
+    -- Apply @beta a b@ recursively to the body of the let expression.
+    else Let bindings (beta a b body)
+
+bindingsContains :: String -> [(String, Expr)] -> Bool
+bindingsContains x bindings = Data.Maybe.isJust $ Data.List.find ((== x) . fst) bindings
