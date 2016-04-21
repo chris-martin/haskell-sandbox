0183539b82b5488abe5068fe0259fa01cceadddd
Chris Martin <ch.martin@gmail.com>
2016-04-20 22:14:42 -0700

    caesar cipher SO question, unfinished

--- src/sandbox.hs
+++ src/sandbox.hs
@@ -1,26 +1,56 @@
+-- | https://stackoverflow.com/questions/36757667/how-do-i-use-ord-and-chr-with-only-a-to-z-and-0-to-9
 module Main (main) where
 
-import           Test.QuickCheck
-import           Test.QuickCheck.Checkers
-import           Test.QuickCheck.Classes
+import           Control.Monad (guard)
+import           Data.Maybe    (fromJust)
+import           Data.Word     (Word8)
 
-import           Data.Monoid              ((<>))
+{-
+  Define a type CaesarChar that corresponds to a valid character
+  and has Ord defined the way you want it.
+-}
+
+newtype CaesarChar = CaesarChar { caesarCharIndex :: Word8 }
+  deriving (Eq, Ord, Show)
+
+fromChar :: Char -> Maybe CaesarChar
+fromChar c | c >= 'A' && c <= 'Z' =
+  caesarChar $ fromEnum c - fromEnum 'A'
+fromChar c | c >= '0' && c <= '9' =
+  caesarChar $ fromEnum c - fromEnum '0' + length ['A'..'Z']
+
+caesarChar :: Integral i => i -> Maybe CaesarChar
+caesarChar i = do
+  let i' = fromIntegral i
+  guard $ i' >= caesarCharIndex minBound
+  guard $ i' <= caesarCharIndex maxBound
+  return $ CaesarChar i'
+
+instance Enum CaesarChar where
+  toEnum = fromJust . caesarChar
+  fromEnum = fromIntegral . caesarCharIndex
+
+instance Bounded CaesarChar where
+  minBound = CaesarChar 0
+  maxBound = CaesarChar $ fromIntegral $ length ['A'..'Z'] + length ['0'..'9']
+
+{-
+  Then the cipher implementation is completely generic.
+-}
+
+-- | Apply the cipher to a single character
+caesar1 :: (Bounded a, Enum a, Integral i) => i -> a -> a
+caesar1 i a = toEnum $ (fromEnum a + fromIntegral i) `mod`
+  (fromIntegral $ fromEnum (maxBound :: CaesarChar))
+
+-- | Apply the cipher to multiple characters.
+caesarN :: (Bounded a, Enum a, Functor f, Integral i) => i -> f a -> f a
+caesarN i = fmap $ caesar1 i
+
+-- | Apply the cipher only to some characters.
+caesarNMaybe :: (Bounded a, Enum a, Functor f, Integral i) =>
+  (b -> Maybe a) -> (a -> b) -> i -> f b -> f a
+caesarNMaybe f i bs = undefined <$> bs
 
 main :: IO ()
-main = return ()
-
-data Tup a b = Tup a b
-
-instance Functor (Tup a) where
-  fmap f (Tup a b) = Tup a (f b)
-
-instance Monoid a => Applicative (Tup a) where
-  (Tup a1 f) <*> (Tup a2 b) = Tup (a1 <> a2) (f b)
-  pure b = Tup mempty b
-
-instance Foldable (Tup a) where
-  foldr f z (Tup _ b) = f b z
-  foldMap f (Tup _ b) = f b
-
-instance Traversable (Tup x) where
-  traverse f (Tup x a) = Tup x <$> f a
+main = putStrLn $ caesarNMaybe caesarChar 2 "ABYZ 0189"
