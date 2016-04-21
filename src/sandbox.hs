b73fbc252d0bda80efc4041a3bc60a43ec48f0a8
Chris Martin <ch.martin@gmail.com>
2016-04-20 18:07:19 -0700

    Tup

--- src/sandbox.hs
+++ src/sandbox.hs
@@ -1,14 +1,26 @@
-module Main (main, factorial) where
+module Main (main) where
 
 import           Test.QuickCheck
 import           Test.QuickCheck.Checkers
 import           Test.QuickCheck.Classes
 
+import           Data.Monoid              ((<>))
+
 main :: IO ()
 main = return ()
 
-factorial :: Integer -> Integer
-factorial n = case compare n 0 of
-  LT -> -1
-  GT -> n * factorial (n-1)
-  EQ -> 1
+data Tup a b = Tup a b
+
+instance Functor (Tup a) where
+  fmap f (Tup a b) = Tup a (f b)
+
+instance Monoid a => Applicative (Tup a) where
+  (Tup a1 f) <*> (Tup a2 b) = Tup (a1 <> a2) (f b)
+  pure b = Tup mempty b
+
+instance Foldable (Tup a) where
+  foldr f z (Tup _ b) = f b z
+  foldMap f (Tup _ b) = f b
+
+instance Traversable (Tup x) where
+  traverse f (Tup x a) = Tup x <$> f a
