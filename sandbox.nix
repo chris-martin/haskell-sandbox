d34b1d312fbed4c8787153ff3ebb7a475c2d7f5d
Chris Martin <ch.martin@gmail.com>
2016-10-27 20:12:03 -0400

    idk

--- sandbox.nix
+++ sandbox.nix
@@ -1,10 +1,12 @@
-{ mkDerivation, base, checkers, QuickCheck, stdenv }:
+{ mkDerivation, base, checkers, QuickCheck, random, safe, stdenv }:
 mkDerivation {
   pname = "sandbox";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base checkers QuickCheck ];
+  executableHaskellDepends = [
+    base checkers QuickCheck random safe
+  ];
   license = stdenv.lib.licenses.asl20;
 }
--- src/sandbox.hs
+++ src/sandbox.hs
@@ -1,56 +1,18 @@
--- | https://stackoverflow.com/questions/36757667/how-do-i-use-ord-and-chr-with-only-a-to-z-and-0-to-9
-module Main (main) where
+module Main (main, Nullable, isNull) where
 
-import           Control.Monad (guard)
-import           Data.Maybe    (fromJust)
-import           Data.Word     (Word8)
+import           Data.IORef
 
-{-
-  Define a type CaesarChar that corresponds to a valid character
-  and has Ord defined the way you want it.
--}
-
-newtype CaesarChar = CaesarChar { caesarCharIndex :: Word8 }
-  deriving (Eq, Ord, Show)
-
-fromChar :: Char -> Maybe CaesarChar
-fromChar c | c >= 'A' && c <= 'Z' =
-  caesarChar $ fromEnum c - fromEnum 'A'
-fromChar c | c >= '0' && c <= '9' =
-  caesarChar $ fromEnum c - fromEnum '0' + length ['A'..'Z']
-
-caesarChar :: Integral i => i -> Maybe CaesarChar
-caesarChar i = do
-  let i' = fromIntegral i
-  guard $ i' >= caesarCharIndex minBound
-  guard $ i' <= caesarCharIndex maxBound
-  return $ CaesarChar i'
-
-instance Enum CaesarChar where
-  toEnum = fromJust . caesarChar
-  fromEnum = fromIntegral . caesarCharIndex
-
-instance Bounded CaesarChar where
-  minBound = CaesarChar 0
-  maxBound = CaesarChar $ fromIntegral $ length ['A'..'Z'] + length ['0'..'9']
-
-{-
-  Then the cipher implementation is completely generic.
--}
-
--- | Apply the cipher to a single character
-caesar1 :: (Bounded a, Enum a, Integral i) => i -> a -> a
-caesar1 i a = toEnum $ (fromEnum a + fromIntegral i) `mod`
-  (fromIntegral $ fromEnum (maxBound :: CaesarChar))
-
--- | Apply the cipher to multiple characters.
-caesarN :: (Bounded a, Enum a, Functor f, Integral i) => i -> f a -> f a
-caesarN i = fmap $ caesar1 i
-
--- | Apply the cipher only to some characters.
-caesarNMaybe :: (Bounded a, Enum a, Functor f, Integral i) =>
-  (b -> Maybe a) -> (a -> b) -> i -> f b -> f a
-caesarNMaybe f i bs = undefined <$> bs
+x :: IO (IORef (Maybe (IORef (Maybe Bool))))
+x = newIORef . Just =<< newIORef Nothing
 
 main :: IO ()
-main = putStrLn $ caesarNMaybe caesarChar 2 "ABYZ 0189"
+main = undefined
+
+data Nullable a = Null | Val a
+
+isNull :: Nullable a -> Bool
+isNull Null = True
+isNull _ = False
+
+fold :: b -> (a -> b) -> Nullable a -> b
+fold = undefined
