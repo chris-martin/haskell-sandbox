12a0300a60a1bb05cde4f376e3ff9e78cdd23ff7
Chris Martin <ch.martin@gmail.com>
2017-08-27 21:01:15 -0400

    update and remove stuff

--- src/sandbox.hs
+++ src/sandbox.hs
@@ -1,11 +1,2 @@
-module Main (main) where
-
 main :: IO ()
-main = mapM_ (putStrLn . show . isPowOf2) [1..10]
-
-isPowOf2 :: Int -> Maybe Int
-isPowOf2 n =
-    if (x `mod` 1.0 /= 0.0) then Nothing
-    else Just (truncate x)
-  where
-    x = logBase (fromIntegral 2) (fromIntegral n)
+main = undefined
