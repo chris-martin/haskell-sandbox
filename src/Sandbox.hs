2bd16680cb9dec843198b64275e548da59c9f589
Chris Martin <ch.martin@gmail.com>
2017-12-15 02:30:06 -0500

    exe -> lib

---
+++ src/Sandbox.hs
@@ -0,0 +1,11 @@
+module Sandbox where
+
+import Numeric.Natural
+
+wrap :: String -> Natural -> String
+wrap = undefined
+
+nextLine :: String -> Natural -> (String, String)
+nextLine = undefined
+
+nextWord :: String -> Natural -> (String, String)
